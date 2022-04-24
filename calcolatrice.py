3f9e61b6a1cab38e418e6b676abf47e0bfb9e9b0
daniodev <103319905+daniodev@users.noreply.github.com>
2022-04-24 11:15:49 +0200

    Add files via upload

---
+++ calcolatrice.py
@@ -0,0 +1,40 @@
+
+a = input('addizione sottrazzione, radice-quadrata, percenuale moltiplicazione o divisione?: ')
+if a == "addizione":
+    print('ok solo un secondo')
+    b = int(input('ok ora scegli il primo numero: '))
+    c = int(input('ok ora scegli il secondo numero: '))
+    d = (b + c)
+    print( d )
+elif a == "sottrazione":
+        print('ok solo un secondo')
+        e = int(input('ok ora scegli il primo numero: '))
+        f = int(input('ok ora scegli il secondo numero: '))
+        h = (e - f)
+        print( h )
+
+elif a == "moltiplicazione":
+    print('ok solo un secondo')
+    i = int(input('ok ora scegli il primo numero: '))
+    l = int(input('ok ora scegli il secondo numero: '))
+    m = (i * l)
+    print( m )
+
+elif a == ("divisione"):
+    print('ok solo un secondo')
+    n = int(input('ok ora scegli il primo numero: '))
+    o = f = int(input('ok ora scegli il secondo numero: '))
+    p = (n / o)
+    print(p)
+
+elif a == ('percenuale'):
+    print('ok solo un secondo')
+    q = int(input('scegli il primo numero: '))
+    r = int(input('ok ora er secondo: '))
+    z = (q / 100 )
+    x = (z * r)
+    print( x )
+
+elif a == ('radice-quadrata'):
+    print('ok solo un secondo')
+    y = int(input(''))
